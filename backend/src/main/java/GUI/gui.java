383c23a0b825e1e3b311af2ddd6f700cef60667f
torzchi <117668600+torzchi@users.noreply.github.com>
2023-12-05 19:41:12 +0200

    namefix

--- backend/src/main/java/GUI/gui.java
+++ backend/src/main/java/GUI/gui.java
@@ -2,7 +2,7 @@ package GUI;
 
 import javax.swing.*;
 
-class GUI {
+class Gui {
     public static void main(String[] args) {
         // Rulează aplicația
         SwingUtilities.invokeLater(new Runnable() {
