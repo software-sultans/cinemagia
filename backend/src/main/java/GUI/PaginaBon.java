93c22afac3f23d798d38e0591a3a2f66bd476803
mihaicr3 <134132369+mihaicr3@users.noreply.github.com>
2023-12-12 21:00:23 +0200

    Bon semifunctional
    AproapeMerge

--- backend/src/main/java/GUI/PaginaBon.java
+++ backend/src/main/java/GUI/PaginaBon.java
@@ -9,43 +9,156 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.sql.Connection;
+
+import java.sql.Statement;
 
 public class PaginaBon extends JFrame{
-    private JFrame frame;
+
+    String[] items=new String[10];
+    int[] idItems=new int[10];
+    int[] cantitate=new int[10];
+    int inceputLabels=10;
+
+    List<Produs> lista=PaginaStoc.getListaProduseFromDatabase();
+    JComboBox<String> comboBox = new JComboBox<>(items);
+
 
     public PaginaBon() {
 
-        frame = new JFrame("Bon");
         // Configurări specifice paginii de stoc
-        frame.setSize(1400, 600);
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.setResizable(false);
+
+        setSize(1400, 600);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setResizable(false);
+        setLayout(null);
+
+         // Butoane
         JButton bauturiButton = new JButton("Bauturi");
         JButton cipsuriButton = new JButton("Cipsuri");
         JButton dulciuriButton = new JButton("Dulciuri");
-        JButton jeleuriButton = new JButton("Extra");
+        JButton filmeButton = new JButton("Filme");
         JButton extraButton = new JButton("Extra");
-        setLayout(null);
+        JButton adaugaButton= new JButton("Adauga");
 
-        bauturiButton.setBounds(10, 80, 100, 25);
 
-        frame.add(bauturiButton);
-        frame.add(cipsuriButton);
-        frame.add(dulciuriButton);
-        frame.add(extraButton);
 
+        bauturiButton.setBounds(10, 10, 100, 25);
+        cipsuriButton.setBounds(110,10,100,25);
+        dulciuriButton.setBounds(210,10,100,25);
+        extraButton.setBounds(310,10,100,25);
+        filmeButton.setBounds(410,10,100,25);
+        adaugaButton.setBounds(10,300,100,25);
+
+
+
+
+
+        comboBox.setBounds(10,50,100,25);
+
+
+        JTextField textField=new JTextField("Cantitate");
+        textField.setBounds(210,50,100,25);
+
+
+        // Action listeners butoane
+
+        bauturiButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                putNameInComboBox("Bauturi");
+
+            }
+        });
+
+        cipsuriButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                putNameInComboBox("Cipsuri");
+
+            }
+        });
+
+        dulciuriButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                putNameInComboBox("Dulciuri");
+
+            }
+        });
+        adaugaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JLabel label1=new JLabel(comboBox.getSelectedItem().toString());
+                label1.setBounds(1120,inceputLabels,200,50);
+
+                JLabel jLabel=new JLabel(textField.getText().concat("x"));
+                jLabel.setBounds(1100,inceputLabels,200,50);
+
+                inceputLabels=inceputLabels+25;
+
+
+//
+//                String updateStocQuery = "UPDATE stoc_produse sp " +
+//                        "INNER JOIN produse p ON sp.id_produs = p.id " +
+//                        "SET sp.pret = " + pret + ", sp.cantitate = " + cantitate + ", sp.data_expirare = '" + dataExpirare + "' " +
+//                        "WHERE p.nume = '" + numeProdus + "'";
+//                statement.executeUpdate(updateStocQuery);
+
+                add(label1);
+                add(jLabel);
+                repaint();
+            }
+        });
+
+        // Adaugare butoane
+        add(bauturiButton);
+        add(cipsuriButton);
+        add(dulciuriButton);
+        add(extraButton);
+        add(filmeButton);
+        add(adaugaButton);
+        add(comboBox);
+        add(textField);
 
     }
 
     public void afiseazaPaginaBon() {
-        frame.setVisible(true);
+
+
+        setVisible(true);
     }
+
+    private void putNameInComboBox(String cuvant)
+    {
+
+        int i=0;
+        for (i=0;i<10;i++)
+        {
+            items[i]="";
+            idItems[i]=0;
+            cantitate[i]=0;
+        }
+        i=0;
+        for(Produs produs:lista)
+        {
+
+            if(produs.getTipProdus().equals(cuvant)) {
+                items[i] = produs.getNume();
+                cantitate[i]=produs.getCantitateDisponibila();
+                i++;
+            }
+        }
+        comboBox.setModel(new DefaultComboBoxModel<>(items));
+    }
+
 
 
 }
